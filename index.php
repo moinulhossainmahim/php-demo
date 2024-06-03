eb32ef1dc7a06bb6a00b0001a5676ac390f24287
mhossain2 <moinulhossainmahim@gmail.com>
2024-06-03 15:50:52 +0600

    added views and partials

--- index.php
+++ index.php
@@ -1,16 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Php For Dummies</title>
-  </head>
-  <body>
-    <h1>
-      <?php
-        $greetings = 'Welcome';
-        echo "$greetings from php!";
-      ?>
-    </h1>
-  </body>
-</html>
+<?php
+  $heading = 'Home';
+
+  require 'views/index.view.php';
---
+++ views/contact.view.php
@@ -0,0 +1,9 @@
+<?php require('views/partials/head.php') ?>
+  <?php require('views/partials/nav.php') ?>
+  <?php require('views/partials/banner.php') ?>
+    <main>
+      <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
+        <h1>Contact with us!</h1>
+      </div>
+    </main>
+<?php require('views/partials/footer.php') ?>
