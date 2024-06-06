7cc6c94af91396ffeed40a12a2b3138b10999792
mhossain2 <moinulhossainmahim@gmail.com>
2024-06-06 21:40:12 +0600

    added

---
+++ views/404.php
@@ -0,0 +1,13 @@
+<?php require('views/partials/head.php') ?>
+  <?php require('views/partials/nav.php') ?>
+
+  <main>
+    <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
+      <h1>
+        Sorry not found!
+      </h1>
+      <a href="/">Go back</a>
+    </div>
+  </main>
+
+<?php require('views/partials/footer.php') ?>
