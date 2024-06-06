1c99cfef478f0f0dfffb7a5db32f5f8836591586
mhossain2 <moinulhossainmahim@gmail.com>
2024-06-06 22:27:16 +0600

    added db connection

--- index.php
+++ index.php
@@ -1,5 +1,14 @@
 <?php
 
   require 'functions.php';
-  require 'router.php';
+  // require 'router.php';
 
+  $dsn = "mysql:host=localhost;port=3306;dbname=demo;password=Mysql123@;user=root;charset=utf8mb4";
+
+  $pdo =  new PDO($dsn);
+
+  $statement = $pdo->prepare("select * from posts");
+  $statement->execute();
+
+  $posts = $statement->fetchAll();
+  dd($posts);
