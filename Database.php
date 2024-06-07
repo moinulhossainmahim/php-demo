5118ec357c701c5b9f8e5dc6ce2028e2b830e1a4
mhossain2 <moinulhossainmahim@gmail.com>
2024-06-07 20:39:14 +0600

    feat: updated db class by custom methods

--- Database.php
+++ Database.php
@@ -1,6 +1,7 @@
 <?php
     class Database {
     public $connection;
+    public $statement;
 
     function __construct($config)
     {
@@ -13,10 +14,31 @@
 
     function query($query, $params = [])
     {
-      $statement = $this->connection->prepare($query);
-      $statement->execute($params);
+      $this->statement = $this->connection->prepare($query);
+      $this->statement->execute($params);
 
-      return $statement;
+      return $this;
+    }
+
+    function get()
+    {
+      return $this->statement->fetchAll();
+    }
+
+    function find()
+    {
+      return $this->statement->fetch();
+    }
+
+    function findOrFail()
+    {
+      $result = $this->find();
+
+      if (! $result) {
+        abort();
+      }
+
+      return $result;
     }
 
   }
