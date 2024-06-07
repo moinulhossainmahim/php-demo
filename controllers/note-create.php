1ee08ece027b611d7ac70246c8ad78ed4e752ac9
mhossain2 <moinulhossainmahim@gmail.com>
2024-06-07 22:46:31 +0600

    feat: added validation in client and server both levels

--- controllers/note-create.php
+++ controllers/note-create.php
@@ -1,8 +1,27 @@
 <?php
   $heading = "Let's create a new note";
+  $config = require('config.php');
+  $db = new Database($config['database']);
 
   if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    dd($_POST);
+
+    $errors = [];
+
+    if(strlen($_POST['body']) === 0) {
+      $errors['body'] = 'A body is required';
+    }
+
+    if(strlen($_POST['body']) > 1000) {
+      $errors['body'] = 'The body can not be more than 1,000 characters';
+    }
+
+    if(empty($errors)) {
+      $db->query('INSERT INTO notes(body, user_id) values(:body, :user_id)', [
+        'body' => $_POST['body'],
+        'user_id' => 1,
+      ]);
+    }
+
   }
 
   require 'views/note-create.view.php';
