c21f672be0cc425cf2f261193c219c080658b110
mhossain2 <moinulhossainmahim@gmail.com>
2024-06-09 16:26:01 +0600

    refactor: updated 404 and 403 views

--- views/403.php
+++ views/403.php
@@ -1,5 +1,5 @@
-<?php require('views/partials/head.php') ?>
-  <?php require('views/partials/nav.php') ?>
+<?php view('partials/head.php') ?>
+<?php view('partials/nav.php') ?>
 
   <main>
     <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
@@ -10,4 +10,4 @@
     </div>
   </main>
 
-<?php require('views/partials/footer.php') ?>
+<?php view('partials/footer.php') ?>
