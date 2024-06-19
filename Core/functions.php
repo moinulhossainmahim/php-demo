91dd04f5511fb87fa0ca5cc5b71268425bd4e682
moinulhossainmahim <moinulhossainmahim@gmail.com>
2024-06-19 14:06:51 +0600

    added abort function

--- Core/functions.php
+++ Core/functions.php
@@ -31,3 +31,10 @@
 
       require base_path('views/' . $path);
   }
+
+  function abort($code = 404) {
+    http_response_code($code);
+    require base_path("views/{$code}.php");
+    die();
+  }
+  
